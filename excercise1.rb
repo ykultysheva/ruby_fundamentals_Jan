6ced7a1cb196727c914329040b1af6016d8ffc1d
Yana Kultysheva <ykultysheva@gmail.com>
2016-01-12 14:12:51 -0500

    Excercise 2 started

--- excercise1.rb
+++ excercise1.rb
@@ -1,4 +1,32 @@
-puts 2 + 3
-puts 2
-puts 3
-puts 2 + 3
+# puts 2 + 3
+# puts 2
+# puts 3
+# puts 2 + 3
+#
+#
+# puts 'Hello world'
+# puts "Hellow world"
+#
+#
+# puts "Betty's pie shop"
+# puts "Betty\'s pie shop"
+#
+#
+# puts "Ada lovelace lived for #{1852 - 1815} years."
+
+
+# Escape sequences inside double quotes:
+# \" double quote
+# \\ single backsplash
+# \a bell/alert
+# \b backspace
+# \r carriage return
+# \n newline
+# \s space
+# \t tab
+
+puts "Hello\t\t\world"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1.Hello\n2. World"
+puts "Hello\aWorld"
