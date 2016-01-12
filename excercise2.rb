b94743eecb23b2f4e4eb8f37f59f265ef6a76dca
Yana Kultysheva <ykultysheva@gmail.com>
2016-01-12 15:09:20 -0500

    Exercise 2 completed

---
+++ excercise2.rb
@@ -0,0 +1,22 @@
+# Question One.
+
+def tip(amount)
+  return amount * 0.15
+end
+
+puts tip(55)
+
+#Question Two
+
+puts "My birthday is May " + 24.to_s + "."
+
+#Question Three
+
+def multi
+  return 45628 * 7839
+end
+
+puts "The resut is #{multi}."
+
+#Question 4
+puts "True"
